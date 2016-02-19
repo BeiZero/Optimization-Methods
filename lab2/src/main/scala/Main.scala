2bc6738f265265f4cd390995b55706906d8c5371
Дмитрий <nda85@ya.ru>
2016-02-19 23:49:49 +0400

    Лаба 2 - коммит триумфа и всяческого успеха

--- lab2/src/main/scala/Main.scala
+++ lab2/src/main/scala/Main.scala
@@ -8,35 +8,33 @@ import scala.math._
 object Main {
 
   def main(args: Array[String]): Unit = {
-    println("Общий вид уравнения phi(x)=(x-d)(x-l)")
+    println("Общий вид уравнения phi(x)=(x-d)(x-l)^3")
 
-    def phi(x:Double, d: Double, l: Double): Double = (x - d) * (x - l)
+    def phi(x:Double, d: Double, l: Double): Double = (x - d) * pow((x - l),3)
 
-    val eps = 0.05
+    val eps = 0.005
 
-    def Fib(k: Int): Int = {
-      if (k == 0) {
-        0
-      }
-      else {
-        if (k == 1) {
-          1
-        }
-        else Fib(k-2) + Fib(k-1)
-      }
+    def Fib(k: Int): Int = k match {
+      case 0 => 0
+      case 1 => 1
+      case n => Fib(n-1) + Fib(n-2)
     }
 
-    val lambda = Fib(5)/Fib(6)
+    val lambda_fib:Double = Fib(5)/Fib(6).toDouble
     val lambda_gold = (sqrt(5)-1)/2
 
+    def lambda(g: Boolean) = if (g) lambda_gold else lambda_fib
 
-    val data = Method.run(phi, args(0).toDouble, args(1).toDouble, lambda_gold, eps)
+    val data = Method(phi, args(0).toDouble, args(1).toDouble, lambda(true), eps)
 
-
-    println(f" k\ta\t\tb\t\td0\t\td1\t\td2\t\tx\t\tFx\t\ty\t\tFy")
+    val tableHeader = Array("k","a","b","d0","d1","d2","x","Fx","y","Fy")
+    tableHeader.foreach(s => printf("%1$10s", s))
+    println
     var k = 1
     for (row <- data) {
-      println(f"$k%2d\t${row._1}%2.2f\t${row._2}%2.2f\t${row._3}%2.2f\t${row._4}%2.2f\t${row._5}%2.2f\t${row._6}%2.2f\t${row._7}%2.2f\t${row._8}%2.2f\t${row._9}%2.2f\t")
+      printf("%1$10d", k)
+      row.foreach(f => printf("%1$ 10.5f", f))
+      println
       k += 1
     }
   }
